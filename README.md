ba16ecea7b68bee088e4b067f1304f0f68a26835
hesiyuan <hesiyuan48@gmail.com>
2018-11-13 17:01:22 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+Copyright © 2018 [MARVIN]. Licensed under [Apache 2.0 License].
+
+# Medium Fidelity Prototype
+
+Using node.js backend for simple demonstration, frontend using javascript, html with bootstrap
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+
+### Prerequisites
+
+ensure that nodejs is installed in your computer, also ensure npm is installed
+
+
+### Installing
+
+In your cloned repo,
+
+type
+
+```
+npm install
+```
+
+And following command will start the server.
+
+```
+npm start
+```
+
+Aside: You can optinally view the html without starting a server.
---
+++ app.js
@@ -0,0 +1,53 @@
+/*
+ * (C) Copyright 2014-2015 Kurento (http://kurento.org/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+var path = require('path');
+var url = require('url');
+//var cookieParser = require('cookie-parser')
+var express = require('express');
+//var session = require('express-session')
+var minimist = require('minimist');
+//var ws = require('ws');
+//var fs    = require('fs');
+var https = require('https');
+
+var argv = minimist(process.argv.slice(2), {
+    default: {
+        as_uri: 'https://0.0.0.0:8080/', // application server address
+        ws_uri: 'ws://18.220.89.40:8888/kurento' // life saver! kurento media server
+    }
+});
+
+
+var app = express();
+
+
+/*
+ * Server startup
+ */
+var asUrl = url.parse(argv.as_uri);
+var port = asUrl.port;
+app.listen(process.env.PORT, process.env.IP);
+
+
+// routes
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname + '/public/tutorial.html'));
+});
+
+
+app.use(express.static(path.join(__dirname, 'public')));
