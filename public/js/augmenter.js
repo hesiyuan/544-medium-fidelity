d0b7e58e1f00cf7cc79b3c5d00161107d260a2de
hesiyuan <hesiyuan48@gmail.com>
2018-11-21 00:27:47 -0800

    incoporating drawing

--- public/js/augmenter.js
+++ public/js/augmenter.js
@@ -8,6 +8,9 @@ let augmenter = {
     this.c2 = document.getElementById('c2');
     this.ctx2 = this.c2.getContext('2d');
     this.status = "default";
+    this.img = new Image();   // Create new img element for tasks page
+    this.img.src = "img/tasks.png";
+
     let self = this;
     this.video.addEventListener('play', function() {
       console.log(self.video.videoWidth);
@@ -25,6 +28,8 @@ let augmenter = {
 
     if (this.status === "default") {
       this.computeFrame();
+    } else if(this.status === "tasks") {
+      this.computeTasks();
     } else if (this.status === "triangle") {
       this.computeTriangle();
     } else if(this.status === "grayscale") {
@@ -54,6 +59,40 @@ let augmenter = {
     this.ctx1.lineTo(45, 125);
     this.ctx1.closePath();
     this.ctx1.stroke(); 
+
+    let external = context.getImageData(0, 0, this.width, this.height);
+    //this.ctx1.putImageData(external, 0, 0);
+
+    let frame = this.ctx1.getImageData(0, 0, this.width, this.height); // get data from canvas
+    
+    let l = external.data.length / 4;
+    for (let i = 0; i < l; i++) { // transfer contents
+      let r = external.data[i * 4 + 0];
+      let g = external.data[i * 4 + 1];
+      let b = external.data[i * 4 + 2];
+      if(r || g || b) {
+        frame.data[i * 4 + 0] = r;
+        frame.data[i * 4 + 1] = g;
+        frame.data[i * 4 + 2] = b;
+      }
+    }
+
+    this.ctx2.putImageData(frame, 0, 0);
+
+    return;
+  },
+
+  computeTasks: function() {
+    this.ctx1.drawImage(this.video, 0, 0, this.width, this.height); // draw image onto canvas
+    // draw a triangle
+    // this.ctx1.beginPath();
+    // this.ctx1.moveTo(125, 125);
+    // this.ctx1.lineTo(125, 45);
+    // this.ctx1.lineTo(45, 125);
+    // this.ctx1.closePath();
+    // this.ctx1.stroke(); 
+    this.ctx1.drawImage(this.img, 0, 0, 500, 480);
+
     let frame = this.ctx1.getImageData(0, 0, this.width, this.height); // get data from canvas
     this.ctx2.putImageData(frame, 0, 0);
     return;
@@ -93,6 +132,8 @@ button2.addEventListener('click', event => {
     augmenter.status = "grayscale";
   } else if (select2.value === "default") {
     augmenter.status = "default";
+  } else if(select2.value === "tasks") {
+    augmenter.status = "tasks";
   }
 
 });
